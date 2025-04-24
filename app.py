6aef6274b48fdd6a02abe1b4d68959ebf909f6c5
Surendra030 <surendrababun030@gmail.com>
2025-04-24 22:30:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def calculate_and_save(open_price, yesterday_high, yesterday_low):
 # Get list of stock symbols in batches
 def get_stock_symbols():
 
-    batch_size = max(1, len(stock_symbols) // 50)
+    batch_size = max(1, len(stock_symbols) // 100)
     main_lst = [stock_symbols[i:i + batch_size] for i in range(0, len(stock_symbols), batch_size)]
     return main_lst
 
